420bcd8a7a7d87de190f26a5f5fc48a317777267
Tabo <tabo@eedge.co.jp>
2024-07-04 13:46:02 +0900

    up

--- apps/app1/next.config.mjs
+++ apps/app1/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@repo/ui'],
-  env: {
-    NEXT_PUBLIC_GOOGLESHEET_API: process.env.NEXT_PUBLIC_GOOGLESHEET_API,
-    PRODUCTION_GAS_ENDPOINT: process.env.PRODUCTION_GAS_ENDPOINT,
-  },
+  env: {},
 };
 
 export default nextConfig;
